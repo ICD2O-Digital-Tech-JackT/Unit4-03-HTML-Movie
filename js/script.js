8217f2bc5520203e4626cf7f543a520073731593
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-04-25 14:29:01 +0000

    tfgfdhg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function Display(Result){
+  document.getElementById("Result").innerHTML=Result 
+}
+function Submit(){
+  let Result = ""
+  let Age = document.getElementById("num").value
+  if (Age>=18){
+    Result="You can watch R Rated Movies"
+  } else if (Age>=13){
+    Result="You can watch PG-13 movies"
+  } else if (Age>=10){
+    Result="You can watch PG movies alone"
+  } else{
+    Result="You can watch G movies"
+  }
+  if (Age<0||Math.round(Age)!=Age){
+    Result="Please enter a valid age"
+  }
+  Display(Result)
+}
